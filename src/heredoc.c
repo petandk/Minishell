e574bde5ac24c9f021637504fe1b5c97d89fc384
Raúl Manzanas Trillo <rmanzana@student.42barcelona.com>
2025-04-30 16:49:16 +0200

    nn

--- src/heredoc.c
+++ src/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: rmanzana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 16:19:34 by rmanzana          #+#    #+#             */
-/*   Updated: 2025/04/30 14:16:14 by rmanzana         ###   ########.fr       */
+/*   Updated: 2025/04/30 16:38:21 by rmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,21 +83,32 @@ static t_list	*ft_hdc_helper(char **splited,
 {
 	t_list	*lines;
 	int		i;
+	int		interrupted;
 
 	lines = NULL;
 	i = 1;
 	errno = 0;
-	while (i <= aux->num_brackets)
+	interrupted = 0;
+	while (i <= aux->num_brackets && !interrupted)
 	{
 		if (lines)
 			ft_lstclear(&lines, free);
 		lines = handle_heredoc(splited[i], shell);
 		if (!lines && errno == EINTR)
+		{
+			interrupted = 1;
 			break ;
+		}
 		i++;
 	}
 	signal(SIGINT, old_sig[0]);
 	signal(SIGQUIT, old_sig[1]);
+	if (interrupted)
+	{
+		if (lines)
+			ft_lstclear(&lines, free);
+		return (NULL);
+	}
 	return (lines);
 }
 
@@ -107,6 +118,7 @@ t_list	*ft_heredoc(char *input, t_shell **shell)
 	t_list		*lines;
 	t_heredoc	*aux;
 	void		(*old_sig[2])(int);
+	int			old_errno;
 
 	lines = NULL;
 	if (ft_strstr(input, "<<") == NULL)
@@ -116,7 +128,12 @@ t_list	*ft_heredoc(char *input, t_shell **shell)
 		return (NULL);
 	old_sig[0] = signal(SIGINT, SIG_IGN);
 	old_sig[1] = signal(SIGQUIT, SIG_IGN);
+	old_errno = errno;
 	lines = ft_hdc_helper(splited, aux, old_sig, shell);
+	if (!lines && errno == EINTR)
+		(*shell)->exit_status = 130;
+	else
+		errno = old_errno;
 	clean_heredoc(aux);
 	if (splited)
 		free_split(splited, -1);
